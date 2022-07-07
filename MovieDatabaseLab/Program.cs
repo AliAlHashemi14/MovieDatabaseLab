7edb0a070935d002eae6f14a46fa54a818c8b02a
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-07-06 23:18:56 -0400

    Ali's first commit

---
+++ MovieDatabaseLab/Program.cs
@@ -0,0 +1,43 @@
+﻿using MovieDatabaseLab;
+
+//necessary variables
+bool runProgram = true;
+    List<Movie> movies = new List<Movie>()
+{
+    new Movie("Parasite", "Horror"),
+    new Movie("The Wailing","Horror"),
+    new Movie("The Matrix","Scifi"),
+    new Movie("Interstellar","Scifi"),
+    new Movie("West Side Story", "Drama"),
+    new Movie("The Shawshank Redemption", "Drama"),
+    new Movie("Dragon Ball Z: Super Android 13","Animated"),
+    new Movie("A Silent Voice", "Animated"),
+    new Movie("Rurouni Kenshin: Trust and Betrayal","Animated"),
+    new Movie("Akira","Animated")
+};
+
+//Main loop
+while (runProgram)
+{
+    Console.WriteLine("Welcome to the movie list application!");
+    Console.WriteLine($"There are {movies.Count} movies in this list.");
+    //storing the return string
+    string choice = Validator.GetUserResponse();
+    MovieSearch(choice);
+
+    //get continue loop
+    runProgram = Validator.GetContinue();
+}
+
+//method to search list for category
+void MovieSearch(string choice)
+{
+    foreach (Movie movie in movies)
+    {
+        if(movie.Category.ToLower().Trim() == choice)
+        {
+            Console.WriteLine(movie.Title);
+        }
+    }
+       
+}
---
+++ MovieDatabaseLab/Validator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabaseLab
+{
+    internal class Validator
+    {
+        public static string GetUserResponse()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a movie category.\nDrama\nAnimated\nScifi\nHorror");
+                string choice = Console.ReadLine().ToLower().Trim();
+
+                if (choice.Equals("drama") || choice.Equals("animated") || choice.Equals("scifi") || choice.Equals("horror"))
+                {
+                    return choice;
+ 
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+
+ 
+        }
+
+        public static bool GetContinue()
+        {
+            //default value
+            bool result = true;
+            //check if user wants to continue
+            while (true)
+            {
+                Console.WriteLine("Would you like to continue? (y/n)");
+                string choice = Console.ReadLine().ToLower().Trim();
+
+                //Check user input
+                if (choice == "y")
+                {
+                    result = true;
+                    break;
+                }
+                else if (choice == "n")
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That was invalid. Try again.");
+                }
+
+            }
+            return result;
+
+        }
+    }
+}
